ee0ac2e5854a3c525103da5e41db80400203abc2
Rajeek02 <rajeekmohamed02@gmail.com>
2025-08-04 19:37:34 +0530

    Initial backend commit

--- back-end/index.js
+++ back-end/index.js
@@ -6,12 +6,17 @@ import jobRoutes from './routes/jobRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({ origin: 'https://job-management-2-fe.onrender.com' }));
 
 app.use(express.json());
 
 // Routes
 app.use('/api/jobs', jobRoutes);
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!' });
+});
+
 
 app.get('/', (req, res) => {
   res.send('Job Management API is running');
